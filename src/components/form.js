938c43554819f6212a519010668f6339868e5184
adamu <zimmazone@yahoo.com>
2019-05-05 23:36:24 -0500

    form initial styling

--- src/components/form.js
+++ src/components/form.js
@@ -3,13 +3,30 @@ import React from 'react'
  class Form extends React.Component {
      render() {
           return (
-      <div>
-        <form>
-            <labe>Name</labe>
-            <input type="text" />
-        </form>
-      </div>
-    )
+            <div
+              style={{
+                minWidth: 400,
+                backgroundColor: "	#F5F5F5",
+                padding: 5,
+              }}
+            >
+              <form>
+                <h2>Subscribe</h2>
+                <input type="text" placeholder="First name"/>
+                <br />
+                <br />
+                <input type="email" placeholder="Email address" />
+                <br />
+                <br />
+                <input
+                  style={{
+                    width: 215,
+                  }}
+                  type="submit"
+                />
+              </form>
+            </div>
+          )
      }
  }
 
